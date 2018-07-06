6a3b3a5b82929e2d49a8376ff6e06a4e3156ef5c
barbarakap19 <barbara.andrade1901@gmail.com>
2018-07-06 06:42:15 -0700

    criado o index.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CalculadoraModule } from './calculadora/calculadora.module';
+import { CalculadoraModule } from './calculadora';
 
 
 @NgModule({
